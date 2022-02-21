dacd76f4c0583e0ffa13c50f2e78255558112b71
nilganu <80824183+nilganu@users.noreply.github.com>
2022-02-21 13:45:36 +0530

    Update MainActivity.kt

--- app/src/main/java/com/cahrusat/gitdemotest/MainActivity.kt
+++ app/src/main/java/com/cahrusat/gitdemotest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     //Comment Added
+    //Second Comment Added
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
